2cf20bb9af6470123aaa045d35f5116657bf251a
Alex Al <Alex@al.com>
2026-04-09 00:50:56 +0300

    создаст entity account; создаст account repository; создаст accountDto;

---
+++ account-app/src/main/java/com/bank/accountapp/repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.bank.accountapp.repository;
+
+import com.bank.accountapp.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByUsername(String username);
+
+    List<Account> findAllByUsernameNot(String username);
+}
