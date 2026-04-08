762eb1a64e685e83016959615b2675f98f983acb
Alex Al <Alex@al.com>
2026-04-08 20:55:40 +0300

    init front-app

---
+++ front-app/src/main/java/com/bank/frontapp/FrontAppApplication.java
@@ -0,0 +1,13 @@
+package com.bank.frontapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontAppApplication.class, args);
+    }
+
+}
