44cb8b7aaf2c7ccc565fbbd8380c83c135f9f741
Alex Al <Alex@al.com>
2026-04-09 16:21:27 +0300

    добавит dto для front-app

---
+++ front-app/src/main/java/com/bank/frontapp/dto/transfer/TransferRequestDto.java
@@ -0,0 +1,34 @@
+package com.bank.frontapp.dto.transfer;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class TransferRequestDto {
+
+    @NotNull(message = "Сумма обязательна")
+    @Min(value = 1, message = "Сумма должна быть больше 0")
+    private Integer value;
+
+    @NotBlank(message = "Логин получателя обязателен")
+    private String login;
+
+    public TransferRequestDto() {
+    }
+
+    public @NotBlank(message = "Логин получателя обязателен") String getLogin() {
+        return login;
+    }
+
+    public void setLogin(@NotBlank(message = "Логин получателя обязателен") String login) {
+        this.login = login;
+    }
+
+    public @NotNull(message = "Сумма обязательна") @Min(value = 1, message = "Сумма должна быть больше 0") Integer getValue() {
+        return value;
+    }
+
+    public void setValue(@NotNull(message = "Сумма обязательна") @Min(value = 1, message = "Сумма должна быть больше 0") Integer value) {
+        this.value = value;
+    }
+}
