4c64802acd3b28485f67443c54e8f36d0100a1fb
Alex Al <Alex@al.com>
2026-04-10 02:21:20 +0300

    поправит стиль

--- account-app/src/main/java/com/bank/accountapp/dto/notification/TransferNotificationRequestDto.java
+++ account-app/src/main/java/com/bank/accountapp/dto/notification/TransferNotificationRequestDto.java
@@ -2,7 +2,14 @@ package com.bank.accountapp.dto.notification;
 
 import java.math.BigDecimal;
 
-public record TransferNotificationRequestDto(String fromUsername, String fromName, BigDecimal fromAmountOld, BigDecimal fromAmountNew,
-                                             String toUsername, String toName, BigDecimal toAmountOld, BigDecimal toAmountNew,
-                                             BigDecimal amount) {
+public record TransferNotificationRequestDto(
+        String fromUsername,
+        String fromName,
+        BigDecimal fromAmountOld,
+        BigDecimal fromAmountNew,
+        String toUsername,
+        String toName,
+        BigDecimal toAmountOld,
+        BigDecimal toAmountNew,
+        BigDecimal amount) {
 }
