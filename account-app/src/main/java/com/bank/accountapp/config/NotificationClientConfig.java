b06c5f410429f85ab373c83fae00814e4b571eb4
Alex Al <Alex@al.com>
2026-04-09 10:03:21 +0300

    добавит config account-app

---
+++ account-app/src/main/java/com/bank/accountapp/config/NotificationClientConfig.java
@@ -0,0 +1,48 @@
+package com.bank.accountapp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class NotificationClientConfig {
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientService authorizedClientService
+    ) {
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository,
+                        authorizedClientService);
+        manager.setAuthorizedClientProvider(authorizedClientProvider);
+        return manager;
+    }
+
+    @Bean
+    public WebClient notificationWebClient(
+            OAuth2AuthorizedClientManager authorizedClientManager,
+            @Value("${bank.notification-gateway.base-url}") String notificationServiceBaseUrl,
+            @Value("${spring.security.oauth2.client.registration.account-app.client-id}") String clientId
+    ) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2.setDefaultClientRegistrationId(clientId);
+
+        return WebClient.builder()
+                .baseUrl(notificationServiceBaseUrl)
+                .apply(oauth2.oauth2Configuration())
+                .build();
+    }
+
+}
