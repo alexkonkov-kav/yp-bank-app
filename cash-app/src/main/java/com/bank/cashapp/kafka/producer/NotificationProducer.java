9f14a05e749e85fa598b36a942c2c5c770f44a81
Alex Al <Alex@al.com>
2026-05-03 02:15:58 +0300

    добавит реализацию отправки в kafka; удалит webClient для Notification-app;

---
+++ cash-app/src/main/java/com/bank/cashapp/kafka/producer/NotificationProducer.java
@@ -0,0 +1,39 @@
+package com.bank.cashapp.kafka.producer;
+
+import com.bank.cashapp.dto.NotificationRequestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class NotificationProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public NotificationProducer(KafkaTemplate<String, String> kafkaTemplate,
+                                ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendEditCashNotification(NotificationRequestDto requestDto) {
+        try {
+            log.info("Cash-app. Sending message to kafka");
+            String message = objectMapper.writeValueAsString(requestDto);
+            kafkaTemplate.send("cash-events",
+                    "Ключ: " + LocalDateTime.now(),
+                    message
+            );
+        } catch (JsonProcessingException e) {
+            log.error("Ошибка сериализации NotificationRequestDto в JSON", e);
+        } catch (Exception e) {
+            log.error("Неизвестная ошибка отправки NotificationRequestDto ", e);
+        }
+    }
+}
